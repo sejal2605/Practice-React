f15fd84c85171bc744cf42d52b160774e8d2407f
sejal2605 <104265122+sejal2605@users.noreply.github.com>
2023-07-26 21:57:44 +0530

    Add files via upload

---
+++ Hide,Show and Toggle in react.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App(){
+  let [status,setStatus] = React.useState(false)
+  return(
+    <div className="App">
+      <h1>Hide, Show and Toggle in ReactJs</h1>
+      {
+        status? <h2>Hello World !</h2>:null
+      }
+      
+      {/* <button onClick={()=>setStatus(false)}>HIDE</button>
+      <button onClick={()=>setStatus(true)}>SHOW</button> */}
+
+      <button onClick={()=>setStatus(!status)}>TOGGLE</button>   
+      
+    </div>
+  )
+}
+
+export default App;
