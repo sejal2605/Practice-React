aa030617bb8eb6638f745194d0e9ea12986fab89
sejal2605 <104265122+sejal2605@users.noreply.github.com>
2023-07-27 20:10:00 +0530

    Add files via upload

---
+++ Component_did_mount.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react'
+class App extends React.Component {
+  constructor()
+  {
+    super();
+    console.log("constructor")
+    this.state={name:"Sejal Singh"}
+  }
+  componentDidMount()
+  {
+    console.warn("componentDidMount")
+
+  }
+  render()
+  {
+    console.log("render")
+
+    return (
+      <div className="App">
+        <h1>Component Did Mount Life Cycle Method </h1>
+          <h2>{this.state.name}</h2>
+        <button onClick={()=>this.setState({name:"Sejal"})}>Update</button>
+      </div>
+    );
+  }
+}
+
+export default App;
