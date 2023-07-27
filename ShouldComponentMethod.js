6f2bb4da89d831988045a374a8e82b5b15ef0846
sejal2605 <104265122+sejal2605@users.noreply.github.com>
2023-07-27 21:29:36 +0530

    Add files via upload

---
+++ ShouldComponentMethod.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import React from "react";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate", this.state.count);
+    if (this.state.count < 5) {
+      return true;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Should Component Update {this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Update Counter
+        </button>
+      </div>
+    )
+  }
+}
+
+export default App
